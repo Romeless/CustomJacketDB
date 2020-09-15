e1f16de6fa73a81b3e477660d7b1b7d1eaf57a17
Rama Lesmana <ramarikkylesmana@gmail.com>
2020-09-15 10:15:43 +0700

    some changes

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,15 +1,10 @@
-<?php
-
-namespace App\Controllers;
+<?php namespace App\Controllers;
 
 class Home extends BaseController
 {
 	public function index()
 	{
-		echo view('template/header');
-		echo view("template/navbar");
-		echo view('Home');
-		echo view("template/footer");
+		return view('welcome_message');
 	}
 
 	//--------------------------------------------------------------------
