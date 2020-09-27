01db6c15866e51c91d16871d076ad202a8bbb45c
Rama Lesmana <ramalesmana.1999@gmail.com>
2020-09-27 21:53:54 +0700

    design api change

--- app/Models/TokensModel.php
+++ app/Models/TokensModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class TokenModel extends Model
+class TokensModel extends Model
 {
     protected $table = 'tokens';
     protected $primaryKey = 'id';
