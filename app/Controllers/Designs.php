cdd41541c940403442b3a9569702acc0cf68696c
Rama Lesmana <ramalesmana.1999@gmail.com>
2020-09-30 12:01:00 +0700

    Design API

--- app/Controllers/Designs.php
+++ app/Controllers/Designs.php
@@ -23,18 +23,18 @@ class Designs extends ResourceController
 
     public function create()
     {
-        error_log("design create");
+        //error_log("design create");
 
         $data = $this->request->getPost();
         
-        error_log("Data: ");
-        file_put_contents("php://stderr", print_r($data, true));
+        //error_log("Data: ");
+        //file_put_contents("php://stderr", print_r($data, true));
         
         $validate = $this->validation->run($data, 'design_validation');
         $errors = $this->validation->getErrors();
 
-        file_put_contents("php://stderr", print_r($validate, true));
-        file_put_contents("php://stderr", print_r($errors, true));
+        //file_put_contents("php://stderr", print_r($validate, true));
+        //file_put_contents("php://stderr", print_r($errors, true));
 
         if($errors)
         {
@@ -44,7 +44,7 @@ class Designs extends ResourceController
         $data['createDate'] = date(DATE_FORMAT);
         $data['updateDate'] = date(DATE_FORMAT);
 
-        file_put_contents("php://stderr", print_r($data, true));
+        //file_put_contents("php://stderr", print_r($data, true));
 
         if($this->model->save($data))
         {
@@ -57,6 +57,7 @@ class Designs extends ResourceController
 
         $data = $this->request->getRawInput();
         $data['id'] = $id;
+
         $validate = $this->validation->run($data, 'design_validation');
         $errors = $this->validation->getErrors();
 
@@ -70,12 +71,11 @@ class Designs extends ResourceController
             return $this->fail('design id tidak ditemukan');
         }
 
-        $design = new \App\Entities\Designs();
-        $design->fill($data);
+        $data['updateDate'] = date(DATE_FORMAT);
 
-        if($this->model->save($design))
+        if($this->model->save($data))
         {
-            return $this->respondUpdated($design, 'Design Updated');
+            return $this->respondUpdated($data, 'Design Updated');
         }
 
     }
