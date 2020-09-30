82899204baeccdf240bc94a34f10c207d9019c46
Rama Lesmana <ramalesmana.1999@gmail.com>
2020-09-30 10:56:18 +0700

    Design API

--- app/Controllers/Designs.php
+++ app/Controllers/Designs.php
@@ -21,7 +21,7 @@ class Designs extends ResourceController
     public function create()
     {
         $data = $this->request->getPost();
-        return $this->respondCreated(array($data, json_decode($data, true)));
+        return $data;
 
         $validate = $this->validation->run($data, 'design_validation');
         $errors = $this->validation->getErrors();
